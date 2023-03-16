5913302d0d7e22d913f3e21494960352d642e18e
juwonk1018 <kjon1018@gmail.com>
2023-03-16 19:23:04 +0900

    3/16

--- BOJ/1261.py
+++ BOJ/1261.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-
 import sys
 input = sys.stdin.readline
 
@@ -12,10 +11,25 @@ maze = []
 for _ in range(n):
     maze.append(list(map(int, input().strip())))
     
-visited = [[0] * m for _ in range(n)]
-
+visited = [[-1] * m for _ in range(n)]
+visited[0][0] = 0
 
 cur = [[0,0]]
 
+cnt = 0
 while(cur):
-    cur.pop()
+    x, y = cur.pop()
+    for dx, dy in dxdy:
+        nx, ny = x+dx, y+dy
+        if(0 <= nx < n and 0 <= ny < m):
+            
+            if(visited[nx][ny] == -1 and maze[nx][ny] == 0):
+                visited[nx][ny] = visited[x][y]
+                cur.append([nx,ny])
+                
+            if(maze[x][y] == 0):
+                if(visited[nx][ny] == -1 and maze[nx][ny] == 1):
+                    visited[nx][ny] = visited[x][y] + 1
+                    cur.append([nx,ny])
+    
+print(visited)
