14bb4b651ba0f885e84348159dd883502577b3ba
Juwon Kim <kjon1018@gmail.com>
2023-03-23 15:38:46 +0900

    [unrated] Title: 표 병합, Time: 1.12 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150366. 표 병합/표 병합.py
+++ 프로그래머스/unrated/150366. 표 병합/표 병합.py
@@ -2,7 +2,8 @@
 
 def solution(commands):
     size = 50
-    graph = [['EMPTY'] * (size+1) for _ in range(size+1)]
+    emptyValue = 'EMPTY'
+    graph = [[emptyValue] * (size+1) for _ in range(size+1)]
     
     mergeList = []
     mergeValue = []
@@ -51,32 +52,32 @@ def solution(commands):
                 
                 if(idx1 == -1 and idx2 != -1): # (r2, c2)가 merge된 상태
                     mergeList[idx2] += [[r1, c1]] # 기존의 값에 삽입
-                    if(graph[r1][c1] != 'EMPTY'): # (r1, c1)의 값이 비어있지 않다면 값을 옮김
+                    if(graph[r1][c1] != emptyValue): # (r1, c1)의 값이 비어있지 않다면 값을 옮김
                         mergeValue[idx2] = graph[r1][c1]        
                     
                 elif(idx1 != -1 and idx2 == -1): # (r1, c1)가 merge된 상태
                     mergeList[idx1] += [[r2, c2]]
-                    if(mergeValue[idx1] == 'EMPTY'):
+                    if(mergeValue[idx1] == emptyValue):
                         mergeValue[idx1] = graph[r2][c2]
                     
                 elif(idx1 != -1 and idx2 != -1): # 모두 merge
                     if(idx1 != idx2):
                         mergeList[idx1] += mergeList[idx2]
-                        if(mergeValue[idx1] == 'EMPTY'):
+                        if(mergeValue[idx1] == emptyValue):
                             mergeValue[idx1] = mergeValue[idx2]
 
                         mergeList.pop(idx2)
                         mergeValue.pop(idx2)
                 elif(idx1 == -1 and idx2 == -1): # 둘 다 병합되지 않으면, 새로운 mergeList를 추가
-                    if(graph[r1][c1] != 'EMPTY'):
+                    if(graph[r1][c1] != emptyValue):
                         mergeValue.append(graph[r1][c1])
                     else:
                         mergeValue.append(graph[r2][c2])
                         
                     mergeList.append([[r1,c1], [r2,c2]])
                 
-                graph[r1][c1] = 'EMPTY'
-                graph[r2][c2] = 'EMPTY'
+                graph[r1][c1] = emptyValue
+                graph[r2][c2] = emptyValue
                 
         elif(command == 'UNMERGE'):
             r, c = int(value[0]), int(value[1])
@@ -92,14 +93,12 @@ def solution(commands):
             r, c = int(value[0]), int(value[1])
             
             val = graph[r][c]
-            if(val == 'EMPTY'):
-                ans = 'EMPTY'
+            if(val == emptyValue):
+                val = 'EMPTY'
                 for i in range(len(mergeList)):
                     if([r,c] in mergeList[i]):
-                        ans = mergeValue[i]
-                        break
-                answer.append(ans)
+                        val = mergeValue[i]
+                answer.append(val)
             else:
                 answer.append(val)
-    
     return answer
