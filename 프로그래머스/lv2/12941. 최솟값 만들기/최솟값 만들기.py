a000ac2d3632c9060d142d80eeb433a03514a987
Juwon Kim <kjon1018@gmail.com>
2023-03-27 14:51:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,8 +1,9 @@
 from collections import deque
 def solution(A,B):
     A.sort()
-    B.sort()
+    B.sort(reverse = True)
+    
     answer = 0
     for i in range(len(A)):
-        answer += A[i] * B[len(B) - 1 - i]
+        answer += A[i] * B[i]
     return answer
