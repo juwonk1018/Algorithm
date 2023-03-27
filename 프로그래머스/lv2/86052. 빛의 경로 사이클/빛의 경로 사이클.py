9a8e69eb81055dbe6e8893ce429ec2342c4ad2a0
Juwon Kim <kjon1018@gmail.com>
2023-03-27 19:04:02 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 823.12 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
+++ 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
@@ -1,6 +1,6 @@
 
-            
 def solution(grid):
+    dxdy = [[1,0],[0,1],[-1,0],[0,-1]] # 아래, 오른, 위, 왼
     
     def cycleCheck(ii, jj, kk):
         cur = [ii,jj,kk]
@@ -8,41 +8,29 @@ def solution(grid):
         while(True):
             i, j, k = cur
             check[i][j][k] = 1
-            if(grid[i][j] == 'S'):
-                if(k==0):
-                    cur = [i+1, j, 0]
-                elif(k==1):
-                    cur = [i, j+1, 1]
-                elif(k==2):
-                    cur = [i, j-1, 2]
-                elif(k==3):
-                    cur = [i-1, j, 3]
+            
+            x, y, d = (i+dxdy[k][0])%len(grid), (j+dxdy[k][1])%len(grid[0]), k
+            
+            if(grid[x][y] == 'L'):
+                d = (d+1)%4
+            elif(grid[x][y] == 'R'):
+                d = (d-1)%4
+                
+#             if(grid[i][j] == 'S'):
+#                 cur = [i+dxdy[k][0], j+dxdy[k][1], k]
 
-            elif(grid[i][j] == 'L'):
-                if(k==0):
-                    cur = [i, j+1, 1]
-                elif(k==1):
-                    cur = [i-1, j, 3]
-                elif(k==2):
-                    cur = [i+1, j, 0]
-                elif(k==3):
-                    cur = [i, j-1, 2]
+#             elif(grid[i][j] == 'L'):
+#                 cur = [i+dxdy[(k+1)%4][0], j+dxdy[(k+1)%4][1], (k+1)%4]
 
-            elif(grid[i][j] == 'R'):
-                if(k==0):
-                    cur = [i, j-1, 2]
-                elif(k==1):
-                    cur = [i+1, j, 0]
-                elif(k==2):
-                    cur = [i-1, j, 3]
-                elif(k==3):
-                    cur = [i, j+1, 1]
-
-            cur[0] = cur[0] % len(grid)
-            cur[1] = cur[1] % len(grid[0])
+#             elif(grid[i][j] == 'R'):
+#                 cur = [i+dxdy[(k-1)%4][0], j+dxdy[(k-1)%4][1], (k-1)%4]
+                
+#             cur[0] = cur[0] % len(grid)
+#             cur[1] = cur[1] % len(grid[0])
             
             cnt += 1
             
+            cur = [x,y,d]
             if(check[cur[0]][cur[1]][cur[2]]):
                 if(cur[0] == ii and cur[1] == jj and cur[2] == kk):
                     return cnt
@@ -52,7 +40,7 @@ def solution(grid):
         
     answer = []
     
-    check = [[[0,0,0,0] for _ in range(len(grid[0]))] for _ in range(len(grid))] # U, L, R, D
+    check = [[[0,0,0,0] for _ in range(len(grid[0]))] for _ in range(len(grid))] # 아래, 오른, 위, 왼
              
     for i in range(len(grid)):
         for j in range(len(grid[0])):
