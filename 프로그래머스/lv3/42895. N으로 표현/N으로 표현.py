cf0b16e8569f1563e20f49d4e7af890d356ca3e4
Juwon Kim <kjon1018@gmail.com>
2023-05-13 14:23:26 +0900

    [level 3] Title: N으로 표현, Time: 404.76 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,23 +1,22 @@
 def solution(N, number):
-    if number == 1:
-        return 1
-    set_list = []
-    
-    for cnt in range(1, 9): # 1개부터 8개까지 확인
-        partial_set = set()
-        partial_set.add(int(str(N) * cnt)) # 이어 붙여서 만드는 경우 넣기
-        for i in range(cnt - 1): # (1, n-1) 부터 (n-1, 1)까지 사칙연산
-            for op1 in set_list[i]:
-                for op2 in set_list[-i - 1]:
-                    partial_set.add(op1 + op2)
-                    partial_set.add(op1 * op2)
-                    partial_set.add(op1 - op2)
-                    if op2 != 0:
-                        partial_set.add(op1 / op2)
-        # 만든 집합에 number가 처음 나오는지 확인
-        if number in partial_set:
-            return cnt
-        set_list.append(partial_set)
-        
-        
-    return -1
+    dp = [float("INF")] * 100001
+    cur = set()
+    for i in range(1, 6):
+        num = int(str(N) * i)
+        cur.add(num)
+        dp[num] = i
+
+    for _ in range(3):
+        curList = list(cur)
+        for num in curList:
+            for num2 in curList:
+                nextNumber = [num + num2, abs(num - num2), num * num2, num // num2, num2 // num]
+                for nxt in nextNumber:
+                    if(0 < nxt <= 100000):
+                        dp[nxt] = min(dp[nxt], dp[num] + dp[num2])
+                        if(dp[nxt] <= 8):
+                            cur.add(nxt)
+    if(dp[number] > 8):
+        return -1
+
+    return dp[number]
