b589c1960a1365c56f640423d34a8418a551a340
Juwon Kim <kjon1018@gmail.com>
2023-06-01 12:45:06 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 345.65 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -3,10 +3,9 @@ def f(number):
     for i in range(len(numberToBinary)-1, -1, -1):
         if(numberToBinary[i] == '0'):
             numberToBinary = numberToBinary[:i] + '1' + numberToBinary[i+1:]
-            for j in range(i+1, len(numberToBinary)):
-                if(numberToBinary[j] == '1'):
-                    numberToBinary = numberToBinary[:j] + '0' + numberToBinary[j+1:]
-                    break
+            if(i+1 < len(numberToBinary)):
+                numberToBinary = numberToBinary[:i+1] + '0' + numberToBinary[i+2:]
+            
             return int('0b'+numberToBinary, 2)
 
 def solution(numbers):
