4dbb9372740f36cad243c59b7c194d2f746a4960
Juwon Kim <kjon1018@gmail.com>
2023-06-01 17:46:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,11 +1,14 @@
 import heapq
 
+# 테케가 모든 경우를 거르지 못하는데, insert하는 idx를 같이 heap에 넣어서 visited를 만드는 방식으로 문제 해결 가능
+
 def solution(operations):
     maxHeap = []
     minHeap = []
     
     iCount = 0
     dCount = 0
+    
     for operation in operations:
         op, num = operation.split()
         num = int(num)
