4d07ceaddbf7c45d7b5e3fd9751233cc7ec2f62c
Juwon Kim <kjon1018@gmail.com>
2023-07-24 17:05:25 +0900

    [Gold III] Title: 사다리 조작, Time: 1600 ms, Memory: 118536 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -18,22 +18,26 @@ def checkLadder():
     
     return True
 
-def putLadder(num):
-    
+def putLadder(num, x, y):
+    global answer
     if(checkLadder()):
-        global answer
         answer = min(answer, num)
         return
  
     if(num <= min(2, answer)): # -> 이 조건을 line 32의 맨 뒤에 추가해서 num == 3일 때에도 n*(h+1) cycle을 돌아서 느렸던 것.
-        for i in range(1, n):
-            for j in range(1, h+1):
-                if(ladderPosition[j][i] == 0 and ladderPosition[j][i-1] == 0 and ladderPosition[j][i+1] == 0):
-                    ladderPosition[j][i] = 1
-
-                    putLadder(num + 1)
-
-                    ladderPosition[j][i] = 0
+        for i in range(y, n):
+            if(i == y):
+                for j in range(y, h+1):
+                    if(ladderPosition[j][i] == 0 and ladderPosition[j][i-1] == 0 and ladderPosition[j][i+1] == 0):
+                        ladderPosition[j][i] = 1
+                        putLadder(num + 1, j, i)
+                        ladderPosition[j][i] = 0
+            else:
+                for j in range(1, h+1):
+                    if(ladderPosition[j][i] == 0 and ladderPosition[j][i-1] == 0 and ladderPosition[j][i+1] == 0):
+                        ladderPosition[j][i] = 1
+                        putLadder(num + 1, j, i)
+                        ladderPosition[j][i] = 0
     
 ladderPosition = [[0] * (n+1) for _ in range(h+1)]
 
@@ -45,6 +49,6 @@ for _ in range(m):
 if(m==0):
     answer = 0
 else:
-    putLadder(0)
+    putLadder(0, 1, 1)
 
 print(answer if answer != float("INF") else -1)
