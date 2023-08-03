c043c64ac29e182d4d1426cc7f8e378eccbc294c
Juwon Kim <kjon1018@gmail.com>
2023-08-03 14:43:41 +0900

    [Platinum V] Title: 히스토그램, Time: 168 ms, Memory: 37716 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+stack = []
+result = 0
+
+for i in range(n):        
+    while(len(stack) != 0 and arr[i] < arr[stack[-1]]):
+        temp = stack.pop()
+        if(len(stack) == 0):
+            result = max(arr[temp] * i, result)
+        else:
+            result = max(arr[temp] * (i - stack[-1] - 1), result)
+        #stack의 마지막 index를 포함하지 않고 i도 포함하지 않아 i - stack[-1] - 1
+    
+    stack.append(i)
+
+while(len(stack) != 0):
+    temp = stack.pop()
+    
+    if(len(stack) == 0):
+        result = max(result, arr[temp] * n)
+    else:
+        result = max(result, arr[temp] * (n - 1 - stack[-1])) # 사실상 -(stack[-1] + 1)
+
+print(result)
