498780963f6b06611ddb712ef8b75cfab7340c55
Juwon Kim <kjon1018@gmail.com>
2023-08-09 16:05:21 +0900

    [Silver I] Title: 봄버맨, Time: 128 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -11,8 +11,6 @@ dy = [1, -1, 0, 0]
 for _ in range(r):
     bombTimer.append([3 if element == '3' else -1 for element in list(input().strip().replace("O", '3').replace(".",'0'))])
 
-
-
 def setBomb():
     for i in range(r):
         for j in range(c):
@@ -30,8 +28,7 @@ def boom():
                     if(0 <= nr < r and 0 <= nc < c and bombTimer[nr][nc] != 0):
                         bombTimer[nr][nc] = -1
                 
-for t in range(1, n+1):
-    
+for t in range(1, min(n+1,6)):
     for i in range(r):
         for j in range(c):
             if(bombTimer[i][j] > 0):
@@ -40,13 +37,26 @@ for t in range(1, n+1):
     if(t == 1):
         continue
 
-
     if(t % 2 == 0):
         setBomb()
+        evenTimer = [bombTimer[i][:] for i in range(r)]
+        continue
     
-    else:
-        boom()
+    boom()
+    if(t == 3):
+        timer1 = [bombTimer[i][:] for i in range(r)]
+    
+    if(t == 5):
+        timer2 = [bombTimer[i][:] for i in range(r)]
 
+
+
+if(n > 1 and n % 4 == 1):
+    bombTimer = [timer2[i][:] for i in range(r)]
+elif(n > 1 and n % 4 == 3):
+    bombTimer = [timer1[i][:] for i in range(r)]
+elif(n > 1 and n % 2 == 0):
+    bombTimer = [evenTimer[i][:] for i in range(r)]
 
 for i in range(r):
     for j in range(c):
