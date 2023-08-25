fd461667875a66ffb19fa716db72750cae4a2662
Juwon Kim <kjon1018@gmail.com>
2023-08-25 14:24:46 +0900

    [Gold V] Title: MooTube (Silver), Time: 3280 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,4 +1,7 @@
 # 플로이드-워셜 = 시간초과
+# 미리 정점간의 거리를 계산하는 것은 O(n * (V + E))로 시간 초과가 발생.
+# O(V + E)는 모든 edges를 탐색하면서 모든 vertexs가 queue에 들어가기 때문에 O(V+E) 
+# 하지만, 각 질문마다 k를 넘는지 확인하는 작업은 모든 edge를 
 
 import sys
 from collections import deque
@@ -12,25 +15,27 @@ edges = [[] for _ in range(n+1)]
 for _ in range(n-1):
     p, q, r = map(int, input().split())
 
-    edges[p].append([q,r])
-    edges[q].append([p,r])
+    edges[p].append((q,r))
+    edges[q].append((p,r))
 
         
-for _ in range(Q):
-    k, v = map(int, input().split())
-
-    q = deque([[v, float("INF")]])
+def bfs(k,v):
+    q = deque([v])
     visited = [0] * (n+1)
     visited[v] = 1
 
     num = 0
+
     while(q):
-        cur, usado = q.popleft()
+        cur = q.popleft()
         for dest, nextUsado in edges[cur]:
-            nextUsado = min(usado, nextUsado)
             if(visited[dest] == 0 and nextUsado >= k):
                 visited[dest] = 1
-                q.append([dest, nextUsado])
+                q.append(dest)
                 num += 1
 
-    print(num)
+    return num
+
+for _ in range(Q):
+    k, v = map(int, input().split())
+    print(bfs(k,v))
