7ffd31e65f7084ee703329c44dc1a479099e17e5
Juwon Kim <kjon1018@gmail.com>
2023-09-05 14:15:31 +0900

    [Gold V] Title: 택배 배송, Time: 2236 ms, Memory: 52584 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,32 @@
+import sys, heapq
+
+n, m = map(int, input().split())
+
+costs = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    costs[a].append([b,c])
+    costs[b].append([a,c])
+
+
+def dijkstra():
+    visited = [0] * 2 + [float("INF")] * (n-1)
+    
+    hq = [[0, 1]]
+
+    while(hq):
+        cow, cur = heapq.heappop(hq)
+        
+        if(visited[cur] < cow):
+            continue
+
+        for dest, cost in costs[cur]:
+            total = cow + cost
+            if(visited[dest] > total):
+                visited[dest] = total
+                heapq.heappush(hq, [total, dest])
+
+    print(visited[n])
+
+dijkstra()
