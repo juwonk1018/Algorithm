52b8a05b2e1a6f48627a39b333c117be832ee286
Juwon Kim <kjon1018@gmail.com>
2024-01-10 10:55:47 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,23 @@
+# set을 활용하면 깔끔하게 표기가 가능함
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+party = []
+
+who = set(list(map(int, input().split()))[1:])
+
+for _ in range(m):
+    cur = list(map(int, input().split()))
+    party.append(set(cur[1:]))
+
+lie = [1] * (m)
+
+for _ in range(m):
+    for i in range(m):
+        if(lie[i] and who & party[i]): #아는 사람이 존재한다면
+            lie[i] = 0
+            who = who.union(party[i])
+            break
+print(sum(lie))
