2e7a9c6125ce518545bc8d95d36ed016fa8a7000
Juwon Kim <kjon1018@gmail.com>
2024-03-22 17:15:00 +0900

    [level 3] Title: 불량 사용자, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+def solution(user_id, banned_id):
+    matched_id = defaultdict(set)
+    for uid in user_id:
+        for bid in banned_id:
+            if(len(uid) == len(bid)):
+                matched = True
+                for i in range(len(uid)):
+                    if(bid[i] != "*" and uid[i] != bid[i]):
+                        matched = False
+                        
+                if(matched):
+                    matched_id[bid].add(uid)
+    
+    
+    bannedList = []
+    
+    for bid in banned_id:
+        if(len(bannedList) == 0):
+            for uid in matched_id[bid]:
+                bannedList.append([uid])
+        else:
+            newBannedList = []
+            for lst in bannedList:
+                for uid in matched_id[bid]:
+                    if(uid not in lst):
+                        newBannedList.append(lst + [uid])
+            bannedList = newBannedList  
+    
+    answer = set()
+    for lst in bannedList:
+        answer.add(tuple(sorted(lst)))
+    
+    return len(answer)
