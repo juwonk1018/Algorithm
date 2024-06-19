4f028cf8480f2328f85c7f322684622aec5a282f
Juwon Kim <kjon1018@gmail.com>
2024-06-20 01:26:23 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+
+    
+    let answer = numbers.sort((a,b) => `${b}${a}` - `${a}${b}`).join("");
+    
+    
+    return answer[0] === '0' ? '0' : answer;
+    //return Number(numbers.join("")).toString();   
+}
